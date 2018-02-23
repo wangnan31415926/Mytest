01cacb4a0a878e05be774801d07c3f728f791f54
wangnan <wangnan3.1415926@163.com>
2018-02-23 14:36:09 +0800

    chang samething

--- buttons.c
+++ buttons.c
@@ -14,7 +14,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this library.  If not, see <http://www.wangnan
  */
 
 #include "buttons.h"
